3dc164a228374fc2e8df708ed82d22e39001cf38
yujie <2025575181@qq.com>
2023-02-25 22:27:22 +0800

    add IPN

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+
 
 # Create your views here.
+def login(req):
+    return HttpResponse("aaa")
+
+
+def register(req):
+    return HttpResponse()
