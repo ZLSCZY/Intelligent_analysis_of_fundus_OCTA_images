6c5a1d0845e281e1cd8dcb1241c42ce0577110f3
C-Al-V-In <864665308@qq.com>
2023-03-16 20:16:36 +0800

    index传参

--- Intelligent_analysis_of_fundus_OCTA_images/tools.py
+++ Intelligent_analysis_of_fundus_OCTA_images/tools.py
@@ -14,7 +14,8 @@ def unzip_file(zip_src, dst_dir):
 # new
 def unzip_file(zip_src, dst_dir, index):
     fz = zipfile.ZipFile(zip_src, 'r')
-    print(zip_src)
     for file in fz.namelist():
         fz.extract(file, dst_dir)
+
+    os.rename(dst_dir + "\\" + fz.namelist()[0], dst_dir + "\\" + str(index))
 
